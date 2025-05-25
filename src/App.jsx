f18bbe9c100717294c8a4c6557bd1f8769b2fdf5
Saikatcoder <saikat2018dutta18@gmail.com>
2025-05-25 13:09:08 +0530

    Added new feature or fixed bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
+import { Navigate, Route, Routes } from 'react-router-dom'
 import HomePage from './pages/HomePage'
 import LoginPage from './pages/LoginPage'
 import SignupPage from './pages/SignupPage'
 
 const App = () => {
+  let authUser = null
   return (
    <div className='flex flex-col items-center justify-start'>
     <Routes>
       <Route>
-        <Route path='/' element={<HomePage/>}/>
-        <Route path='/login' element={<LoginPage/>}/>
-        <Route path='/signup' element={<SignupPage/>}/>
+        <Route path='/' element={authUser ? <HomePage/> : <Navigate to = {"/login"}/>}/>
+        <Route path='/login' element={!authUser ? <LoginPage/> : <Navigate to={"/"}/>}/>
+        <Route path='/signup' element={!authUser ? <SignupPage/> : <Navigate to={"/"}/>}/>
       </Route>
     </Routes>
    </div>
